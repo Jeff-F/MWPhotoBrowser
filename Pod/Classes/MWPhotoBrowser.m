a1ceba516ebf9a2f9c5b3db96a8ce42c49b304b1
GT-Jamie.ling <fanzhengfeng@turingcat.com>
2018-06-06 15:11:52 +0800

    compatible safearea

--- Pod/Classes/MWPhotoBrowser.m
+++ Pod/Classes/MWPhotoBrowser.m
@@ -1006,16 +1006,34 @@ static void * MWVideoPlayerObservation = &MWVideoPlayerObservation;
     CGFloat height = 44;
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone &&
         UIInterfaceOrientationIsLandscape(orientation)) height = 32;
-	return CGRectIntegral(CGRectMake(0, self.view.bounds.size.height - height, self.view.bounds.size.width, height));
+    CGFloat adjust = 0;
+    if (@available(iOS 11.0, *)) {
+        //Account for possible notch
+        UIEdgeInsets safeArea = [[UIApplication sharedApplication] keyWindow].safeAreaInsets;
+        adjust = safeArea.bottom;
+    }
+    return CGRectIntegral(CGRectMake(0, self.view.bounds.size.height - height - adjust, self.view.bounds.size.width, height));
 }
 
 - (CGRect)frameForCaptionView:(MWCaptionView *)captionView atIndex:(NSUInteger)index {
     CGRect pageFrame = [self frameForPageAtIndex:index];
     CGSize captionSize = [captionView sizeThatFits:CGSizeMake(pageFrame.size.width, 0)];
-    CGRect captionFrame = CGRectMake(pageFrame.origin.x,
-                                     pageFrame.size.height - captionSize.height - (_toolbar.superview?_toolbar.frame.size.height:0),
-                                     pageFrame.size.width,
-                                     captionSize.height);
+    
+    CGRect captionFrame;
+    if (@available(iOS 11.0, *)) {
+        //Account for possible notch
+        UIEdgeInsets safeArea = [[UIApplication sharedApplication] keyWindow].safeAreaInsets;
+        captionFrame = CGRectMake(pageFrame.origin.x,
+                                  pageFrame.size.height - captionSize.height - (_toolbar.superview?_toolbar.frame.size.height+safeArea.bottom: safeArea.bottom),
+                                  pageFrame.size.width,
+                                  captionSize.height);
+    }else{
+        captionFrame = CGRectMake(pageFrame.origin.x,
+                                  pageFrame.size.height - captionSize.height - (_toolbar.superview?_toolbar.frame.size.height:0),
+                                  pageFrame.size.width,
+                                  captionSize.height);
+    }
+    
     return CGRectIntegral(captionFrame);
 }
 
